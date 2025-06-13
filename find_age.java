b6a3e8fdc539d37c710d71a75e51a152484d2820
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-13 16:28:34 +0530

    [Aman Singh] Add . find_age

---
+++ find_age.java
@@ -0,0 +1,11 @@
+
+public class find_age{
+    public static void main (String []args){
+        int current_y = 2024;
+        int birth_y = 2000;
+        int age = birth_y - current_y;
+        System.out.println(age);
+        
+    }
+
+}
