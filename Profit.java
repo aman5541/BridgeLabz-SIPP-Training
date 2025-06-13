4b883b610ff158d469da3e3db305794d4237d010
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-13 16:31:27 +0530

    [Aman Singh] Add . Profit

---
+++ Profit.java
@@ -0,0 +1,12 @@
+public class Profit {
+    public static void main(String[] args) {
+        int costPrice = 129;
+        int sellingPrice = 191;
+        int profit = sellingPrice - costPrice;
+        double profitPercent = (profit * 100.0) / costPrice;
+
+        System.out.println( profitPercent
+            
+        );
+    }
+}
