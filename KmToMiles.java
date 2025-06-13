026bd5a862be33fa7a897db9ceaa9be4beb9a208
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-13 16:29:28 +0530

    [Aman Singh] Add .  KmToMiles

---
+++ KmToMiles.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double km = input.nextInt();
+        double miles = km / 1.6;
+        System.out.println("Distance in miles: " + miles);
+        input.close();
+    }
+}
