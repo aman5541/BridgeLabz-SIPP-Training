aa2a6110ffad3048b3a7969e8c0833d6b0620054
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-16 09:28:36 +0530

    [Aman Singh] Add . square

---
+++ square.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double perimeter, side;
+
+        System.out.print("Enter perimeter: ");
+        perimeter = in.nextDouble();
+        side = perimeter / 4;
+
+        System.out.printf("The length of the side is %.2f whose perimeter is %.2f", side, perimeter);
+    }
+}
