d8dab5da86535213a5cb09c1131231ba1ab78b99
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-16 09:24:38 +0530

    [Aman Singh] Add . Distance_conversion

---
+++ Distance_conversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Distance_conversion{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double feet, yards, miles;
+
+        System.out.print("Enter distance in feet: ");
+        feet = in.nextDouble();
+
+        yards = feet / 3;
+        miles = yards / 1760;
+
+       System.out.printf("The distance in yards is %.2f while the distance in miles is %.4f", yards, miles);
+    }
+}
