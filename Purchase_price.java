23fa3dcde39971e542738ab734e15b1098579a8f
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-16 09:27:44 +0530

    [Aman Singh] Add . Purchase_price

---
+++ Purchase_price.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Purchase_price {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double unitPrice, total;
+        int quantity;
+
+        System.out.print("Enter unit price: ");
+        unitPrice = in.nextDouble();
+        System.out.print("Enter quantity: ");
+        quantity = in.nextInt();
+
+        total = unitPrice * quantity;
+
+        System.out.printf("The total purchase price is INR %.2f if the quantity %d and unit price is INR %.2f", total, quantity, unitPrice);
+    }
+}
+
