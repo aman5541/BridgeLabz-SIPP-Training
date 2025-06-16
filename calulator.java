29492cd1c181391a57cb6edecea869d09f4e9a38
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-16 09:23:46 +0530

    [Aman Singh] Add . calulator

---
+++ calulator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class calulator {
+    public static void main (String []args){
+        Scanner input = new Scanner(System.in);
+         System.out.println("first number");
+         double input1 = input.nextDouble();
+
+          System.out.println("Operator");
+          char operator = input.next().charAt(0);
+        
+         System.out.println("Second number");
+          double input2 = input.nextDouble();
+
+          ;
+          double result;
+          switch (operator){ 
+
+            case '+':
+            result = input1 + input2;
+            System.out.println(result);
+            break;
+             
+            case '-':
+            result = input1 - input2;
+            System.out.println(result);
+            break;
+
+             case '*':
+            result = input1 * input2;
+            System.out.println(result);
+            break;
+
+          case '/' :
+            result = input1 / input2;
+            System.out.println(result);
+            break;
+
+
+          }
+
+
+
+         
+    }
+
+}
+
