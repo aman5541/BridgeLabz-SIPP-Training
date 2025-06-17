0fab2174e7b30795fa0418aca9317851168b9141
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-17 09:29:26 +0530

    [Aman Singh]  Fix .  Name change

--- Day1_programmingElements/square.java
+++ Day1_programmingElements/square.java
@@ -1,7 +1,7 @@
 package Day1_programmingElements;
 import java.util.Scanner;
 
-public class square {
+public class Square {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double perimeter, side;
