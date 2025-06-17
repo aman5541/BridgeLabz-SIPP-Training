b79dbc4a020c1a33febfaaefaed3ba2d5645270b
aman5541 <122379639+aman5541@users.noreply.github.com>
2025-06-17 17:46:02 +0530

    [Aman Singh] Add . Classwork

---
+++ Day2_ControlFlow/Level1/Example2.java
@@ -0,0 +1,12 @@
+package Day2_ControlFlow.Level1;
+
+class Example2 {
+   static void display() {
+        System.out.println("static Method");
+    }
+
+    public static void main (String[] args) {
+       
+          display();
+    }
+}
