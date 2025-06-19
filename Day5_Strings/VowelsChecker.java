3d7112fbce5d9f30bec65c27916ea03be0a20639
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-06-19 11:32:09 +0530

    [Aman Singh] Add . VowelsChecker

---
+++ Day5_Strings/VowelsChecker.java
@@ -0,0 +1,49 @@
+package Day5_Strings;
+
+import java.util.Scanner;
+
+public class VowelsChecker {
+
+    
+    public static int charType(char c) {
+        char lower = Character.toLowerCase(c);
+        if (lower >= 'a' && lower <= 'z') {
+            if (lower=='a' || lower=='e' || lower=='i' || lower=='o' || lower=='u') {
+                return 1;  // Vowel
+            } else {
+                return 2;  // Consonant
+            }
+        }
+        return 0;  // Not a letter
+    }
+
+   
+    public static int[] countVowelsAndConsonants(String input) {
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            int type = charType(input.charAt(i));
+            if (type == 1) {
+                vowelCount++;
+            } else if (type == 2) {
+                consonantCount++;
+            }
+        }
+        return new int[]{vowelCount, consonantCount};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String userInput = scanner.nextLine();
+
+        int[] counts = countVowelsAndConsonants(userInput);
+
+        System.out.println("Number of vowels: " + counts[0]);
+        System.out.println("Number of consonants: " + counts[1]);
+
+        
+    }
+}
+
