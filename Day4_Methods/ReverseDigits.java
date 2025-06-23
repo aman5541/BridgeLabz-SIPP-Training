46e9507dc06522d24296465328e2936429e77688
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-06-23 09:20:28 +0530

    [Aman Singh] Add . ReverseDigits

---
+++ Day4_Methods/ReverseDigits.java
@@ -0,0 +1,19 @@
+package Day4_Methods;
+import java.util.Scanner;
+public class ReverseDigits {
+    public static int reverseNumber(int number) {
+        int reverse = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+        return reverse;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Reversed number: " + reverseNumber(num));
+    }
+}
