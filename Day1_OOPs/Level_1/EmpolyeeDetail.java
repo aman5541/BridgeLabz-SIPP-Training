6359e9093e17408446e4e54f1138cba3e1e2b12a
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-06-24 23:43:20 +0530

    [Aman Singh]  Add . EmpolyeeDetail

---
+++ Day1_OOPs/Level_1/EmpolyeeDetail.java
@@ -0,0 +1,21 @@
+class EmpolyeeDetail{
+    int id;
+    String name;
+    int salary;
+    
+
+    EmpolyeeDetail(int id;String name;int salary){
+      this.id = id;
+      this.name= name;
+      this.salary = salary;
+    }
+    void display(){
+       System.out.println("Empolyee Id :" + id);
+    System.out.println("Empolyee Name :" + name);
+    System.out.println("Empolyee Salary :" + salary);
+    }
+    Public static void main(String[]args){
+        EmpolyeeDetail emp = new EmpolyeeDetail(101,Aman singh,50000);
+        emp.dispay();
+    }
+}
