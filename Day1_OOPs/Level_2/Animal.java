d6bcdc0afc0597ff3594b77909867d349fe188a4
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-06-26 23:21:57 +0530

    [Aman Singh]  Add .  Animal  class work

---
+++ Day1_OOPs/Level_2/Animal.java
@@ -0,0 +1,25 @@
+
+    
+
+   public class Animal { 
+      
+      int age = 19;
+      static int age1 =20;
+           static void display(){
+            System.out.println("static ");
+
+           
+           }
+            void display1(){
+               System.out.println(" Non static ");
+            }
+           
+            public static void main(String[]args){
+               Animal animal=new Animal();
+               animal.display1();
+               display();
+               System.out.println(animal.age);
+                System.out.println(age1);
+            }
+
+   }
