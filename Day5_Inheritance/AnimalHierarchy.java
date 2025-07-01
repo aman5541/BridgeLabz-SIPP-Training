cf5f11a407df124416b9d480a1dd4702cae3fb68
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-01 13:09:17 +0530

    [Aman Singh] Add . AnimalHierarchy

---
+++ Day5_Inheritance/AnimalHierarchy.java
@@ -0,0 +1,56 @@
+class Animal {
+    String name;
+    int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    void makeSound() {
+        System.out.println(name + " says: Woof!");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    void makeSound() {
+        System.out.println(name + " says: Meow!");
+    }
+}
+
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    void makeSound() {
+        System.out.println(name + " says: Tweet!");
+    }
+}
+
+public class AnimalHierarchy {
+    public static void main(String[] args) {
+        Animal myDog = new Dog("Buddy", 3);
+        Animal myCat = new Cat("Whiskers", 2);
+        Animal myBird = new Bird("Tweety", 1);
+
+        myDog.makeSound();
+        myCat.makeSound();
+        myBird.makeSound();
+    }
+}
+
