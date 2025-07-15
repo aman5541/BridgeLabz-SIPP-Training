f3db3399f3d40f2a936d331d83a1977bf71a154a
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-15 12:42:28 +0530

    [Aman Singh]  Add .  graduate

---
+++ WorkShop/UniversityEnrollment/Undergraduate.java
@@ -0,0 +1,13 @@
+package com.gla;
+
+class Undergraduate extends Student {
+	
+	
+    public Undergraduate(String name, int id) {
+        super(name, id);
+    }
+
+    public Undergraduate(String name, int id, String elective) {
+        super(name, id, elective);
+    }
+}
