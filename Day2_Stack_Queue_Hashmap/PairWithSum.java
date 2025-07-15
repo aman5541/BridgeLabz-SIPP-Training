8f689116db15898dc9d91b6a9bdb0808ba20fe71
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-15 13:23:58 +0530

    [Aman Singh]  Add .  PairWithSum

---
+++ Day2_Stack_Queue_Hashmap/PairWithSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PairWithSum {
+    static boolean hasPair(int[] arr, int target) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : arr) {
+            if (seen.contains(target - num)) return true;
+            seen.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+        System.out.println(hasPair(arr, target));
+    }
+}
