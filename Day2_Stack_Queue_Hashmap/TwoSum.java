a6bab497e8816878e874ea30797652f304abad62
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-15 13:22:27 +0530

    [Aman Singh]  Add . TwoSum

---
+++ Day2_Stack_Queue_Hashmap/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class TwoSum {
+    static int[] findTwoSum(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (map.containsKey(complement))
+                return new int[]{map.get(complement), i};
+            map.put(arr[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int[] result = findTwoSum(arr, 9);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
