e3103ea24754dea472ffb27ecd474ba4b7b989cb
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-15 12:41:39 +0530

    [Aman Singh]  Add . Postgraduate

---
+++ WorkShop/UniversityEnrollment/Postgraduate.java
@@ -0,0 +1,11 @@
+package com.gla;
+
+class Postgraduate extends Student {
+    public Postgraduate(String name, int id) {
+        super(name, id);
+    }
+
+    public Postgraduate(String name, int id, String elective) {
+        super(name , id, elective);
+    }
+} 
