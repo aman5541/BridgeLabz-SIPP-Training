0d4fcd20403aa3afdea6978020e771cae5fc6b4a
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-21 12:45:21 +0530

    [Aman Singh] Add . FindPeakElement

---
+++ Day4_Linear and Binary Search/FindPeakElement.java
@@ -0,0 +1,18 @@
+public class FindPeakElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if ((mid == 0 || arr[mid] >= arr[mid - 1]) &&
+                (mid == arr.length - 1 || arr[mid] >= arr[mid + 1])) {
+                System.out.println("Peak: " + arr[mid]);
+                return;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+}
