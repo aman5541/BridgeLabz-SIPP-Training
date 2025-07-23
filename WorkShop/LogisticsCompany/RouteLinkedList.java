8a1e25f1ced74ea186c32e71b18bf1a5d3f92a04
Aman Singh <aman.singh5_cs22@gla.ac.in>
2025-07-23 09:55:54 +0530

    [Aman Singh]  Add . RouteLinkedList

---
+++ WorkShop/LogisticsCompany/RouteLinkedList.java
@@ -0,0 +1,90 @@
+package WorkShop.LogisticsCompany;
+
+public class RouteLinkedList<T extends Checkpoint> {
+    private class Node {
+        T data;
+        Node next;
+        Node(T data) { this.data = data; }
+    }
+    private Node head;
+
+    public void addCheckpoint(T checkpoint) {
+        Node newNode = new Node(checkpoint);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    public boolean removeCheckpoint(String checkpointId) {
+        if (head == null) return false;
+        if (head.data.getCheckpointId().equals(checkpointId)) {
+            head = head.next;
+            return true;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.data.getCheckpointId().equals(checkpointId)) {
+                temp.next = temp.next.next;
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public T findCheckpoint(String checkpointId) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.getCheckpointId().equals(checkpointId)) return temp.data;
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public double computeTotalDistance() {
+        double total = 0;
+        Node temp = head;
+        while (temp != null) {
+            total += temp.data.getDistanceFromLast();
+            temp = temp.next;
+        }
+        return total;
+    }
+
+    public double computeTotalPenalty() {
+        double total = 0;
+        Node temp = head;
+        while (temp != null) {
+            total += temp.data.calculatePenalty();
+            temp = temp.next;
+        }
+        return total;
+    }
+
+    public void printRoute() {
+        Node temp = head;
+        int i = 1;
+        while (temp != null) {
+            Checkpoint cp = temp.data;
+            System.out.printf("%d. %s - %s - %s - Penalty: %.1f\n", i++, cp.getType(), cp.getLocationName(), cp.isDelayed() ? "Delayed" : "On Time", cp.calculatePenalty());
+            temp = temp.next;
+        }
+    }
+
+    public boolean checkCriticalConsistency() {
+        boolean hasDelivery = false, hasFuel = false;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.isCritical()) {
+                if (temp.data instanceof DeliveryCheckpoint) hasDelivery = true;
+                if (temp.data instanceof FuelCheckpoint) hasFuel = true;
+            }
+            temp = temp.next;
+        }
+        return hasDelivery && hasFuel;
+    }
+}
